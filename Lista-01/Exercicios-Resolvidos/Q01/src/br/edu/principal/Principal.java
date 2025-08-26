1aa44ac47c8820ae8e959002aa594f7a4538a8fb
Elisa <152522410+elhisa@users.noreply.github.com>
2025-08-26 10:27:19 -0300

    Add files via upload

---
+++ Lista-01/Exercicios-Resolvidos/Q01/src/br/edu/principal/Principal.java
@@ -0,0 +1,18 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1, n2 , n3, n4 , soma;
+		n1 = 1;
+		n2 = 2;
+		n3 = 3;
+		n4 = 4;
+		
+		soma = n1 + n2 + n3 + n4;
+		
+		System.out.println(soma);
+	}
+
+}
