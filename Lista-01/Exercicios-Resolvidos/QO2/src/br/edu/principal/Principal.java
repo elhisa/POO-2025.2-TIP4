ddcad48ce758693b09f0d32eafb4ce4b7d70801b
Elisa <152522410+elhisa@users.noreply.github.com>
2025-08-26 10:46:01 -0300

    Add files via upload

---
+++ Lista-01/Exercicios-Resolvidos/QO2/src/br/edu/principal/Principal.java
@@ -0,0 +1,17 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int nota1, nota2, nota3, media;
+        nota1 = 1;
+        nota2 = 2;
+        nota3 = 3;
+        
+        media = nota1 + nota2 + nota3;
+        
+        System.out.println(media);
+	}
+
+}
