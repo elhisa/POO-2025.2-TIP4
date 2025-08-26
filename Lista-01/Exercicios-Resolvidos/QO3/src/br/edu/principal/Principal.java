3ed8d8765fc63344d0fca329d5e6d52b2730134d
Elisa <152522410+elhisa@users.noreply.github.com>
2025-08-26 10:58:34 -0300

    Add files via upload

---
+++ Lista-01/Exercicios-Resolvidos/QO3/src/br/edu/principal/Principal.java
@@ -0,0 +1,23 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nota1, nota2, nota3, peso1, peso2, peso3, media;
+		nota1 = 1;
+		nota2 = 2;
+		nota3 = 3;
+		peso1 = 1;
+		peso2 = 2;
+		peso3 = 3;
+		
+		media = nota1 + nota2 + nota3 + peso1 + peso2 + peso3;
+		
+		System.out.println(media);
+			
+		
+
+	}
+
+}
